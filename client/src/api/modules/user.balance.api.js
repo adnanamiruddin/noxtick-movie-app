bea2fa6a6756eb2726422bfe2dd7a0e2638f17cb
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-07-03 00:22:41 +0800

    feat: Create user balance api module

---
+++ client/src/api/modules/user.balance.api.js
@@ -0,0 +1,43 @@
+import privateClient from "../client/private.client";
+
+const userBalanceEndPoints = {
+  balance: "user/balance/balance",
+  topup: "user/balance/topup",
+  withdraw: "user/balance/withdraw",
+};
+
+const userBalanceApi = {
+  getBalance: async () => {
+    try {
+      const response = await privateClient.get(userBalanceEndPoints.balance);
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  },
+
+  topup: async ({ amount }) => {
+    try {
+      const response = await privateClient.post(userBalanceEndPoints.topup, {
+        amount,
+      });
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  },
+
+  withdraw: async ({ amount, password }) => {
+    try {
+      const response = await privateClient.post(userBalanceEndPoints.withdraw, {
+        amount,
+        password,
+      });
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  }
+};
+
+export default userBalanceApi;
