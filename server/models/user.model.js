50ee0a59f1b68dac272dd8ca5ca0bc4800139484
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-07-02 15:06:30 +0800

    feat: Create user.model and implement model.options

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,3 +1,49 @@
 import mongoose from "mongoose";
-import crypro from "crypto"
+import crypto from "crypto";
+import modelOptions from "./model.options.js";
 
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    displayName: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+  },
+  modelOptions
+);
+
+userSchema.methods.setPassword = function (password) {
+  this.salt = crypto.randomBytes(16).toString("hex");
+  this.password = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
+    .toString("hex");
+};
+
+userSchema.methods.validatePassword = function (password) {
+  const hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
+    .toString("hex");
+  return this.password === hash;
+};
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
