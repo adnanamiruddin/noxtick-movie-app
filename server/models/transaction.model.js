bb3a2a4e6d12a66b9386e1b648235e92b723e5c9
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-07-02 16:08:12 +0800

    feat: Create model for transaction

---
+++ server/models/transaction.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import modelOptions from "./model.options.js";
+
+const transactionSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  ticket: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Ticket",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  transactionDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  transactionStatus: {
+    type: String,
+    required: true,
+    enum: ["Pending", "Paid", "Cancelled"],
+    default: "Pending",
+  },
+});
+
+const transactionModel = mongoose.model("Transaction", transactionSchema);
+
+export default transactionModel;
