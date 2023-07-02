268c52612298244bc594a5569111a8e5a849d68c
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-07-02 14:37:53 +0800

    feat: Install and initialize project on server side (BackEnd)

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import http from "http";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+
+const port = process.env.PORT || 8082;
+const server = http.createServer(app);
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("MongoDb Connected bang");
+    server.listen(port, () => {
+      console.log(`Server is listening on ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log({ err });
+    process.exit(1);
+  });
