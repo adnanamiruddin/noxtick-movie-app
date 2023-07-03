61e7bc736d23f48861df4097cec5939bf4dde324
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-07-03 14:49:38 +0800

    feat: Add all pages to their route in routes.jsx

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,3 +1,12 @@
+import ProtectedPage from "../components/common/ProtectedPage";
+import AboutPage from "../pages/AboutPage";
+import HomePage from "../pages/HomePage";
+import MovieDetail from "../pages/MovieDetail";
+import TopupBalance from "../pages/TopupBalance";
+import Transactions from "../pages/Transactions";
+import UserInfo from "../pages/UserInfo";
+import WithdrawBalance from "../pages/WithdrawBalance";
+
 export const routesGen = {
   home: "/",
   about: "/about",
@@ -7,3 +16,59 @@ export const routesGen = {
   topup: "/topup",
   withdraw: "/withdraw",
 };
+
+const routes = [
+  {
+    index: true,
+    element: <HomePage />,
+    state: "home",
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+    state: "about",
+  },
+  {
+    path: "/movie/:movieId",
+    element: <MovieDetail />,
+    state: "movie.detail",
+  },
+  {
+    path: "/my-info",
+    element: (
+      <ProtectedPage>
+        <UserInfo />
+      </ProtectedPage>
+    ),
+    state: "info",
+  },
+  {
+    path: "/transactions",
+    element: (
+      <ProtectedPage>
+        <Transactions />
+      </ProtectedPage>
+    ),
+    state: "transactions",
+  },
+  {
+    path: "/topup",
+    element: (
+      <ProtectedPage>
+        <TopupBalance />
+      </ProtectedPage>
+    ),
+    state: "topup",
+  },
+  {
+    path: "/withdraw",
+    element: (
+      <ProtectedPage>
+        <WithdrawBalance />
+      </ProtectedPage>
+    ),
+    state: "withdraw",
+  },
+];
+
+export default routes;
