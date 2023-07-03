cb44bd96d94aae11d8e5d1f51e63e18ffc6683df
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-07-03 14:13:39 +0800

    feat: Configuration React Toast and CssBaseline from Material UI in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,39 @@
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import MainLayout from "./components/layout/MainLayout";
 
 const App = () => {
   return (
     <div>
-      <h1>React App</h1>
-    </div>
-  )
-}
+      {/* Config React Toastify START */}
+      <ToastContainer
+        position="bottom-left"
+        autoClose={3500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        theme="dark"
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+      />
+      {/* Config React Toastify END */}
 
-export default App
+      {/* Material UI reset CSS START */}
+      <CssBaseline />
+      {/* Material UI reset CSS END */}
+
+      {/* App Routest START */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* App Routest END */}
+    </div>
+  );
+};
+
+export default App;
