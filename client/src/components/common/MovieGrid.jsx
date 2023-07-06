e7de0816541b23ba922b5e050a09c722cba23b93
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-07-07 00:28:50 +0800

    feat: Displays all movies on MoviesPage using component MoviesGrid

--- client/src/components/common/MovieGrid.jsx
+++ client/src/components/common/MovieGrid.jsx
@@ -1,12 +1,12 @@
 import { Grid } from "@mui/material";
 import MovieItem from "./MovieItem";
 
-const MovieGrid = ({ medias }) => {
+const MovieGrid = ({ movies }) => {
   return (
     <Grid container spacing={1} sx={{ marginRight: "-8px !important" }}>
-      {medias.map((media, i) => (
+      {movies.map((movie, i) => (
         <Grid key={i} item xs={6} sm={4} md={3}>
-          <MovieItem media={media} />
+          <MovieItem movie={movie} />
         </Grid>
       ))}
     </Grid>
--- client/src/pages/MoviesPage.jsx
+++ client/src/pages/MoviesPage.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { Box, Toolbar } from "@mui/material";
+import uiConfigs from "../configs/ui.configs";
+import MovieGrid from "../components/common/MovieGrid";
+import { useEffect, useState } from "react";
+import movieApi from "../api/modules/movie.api";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { setGlobalLoading } from "../redux/features/globalLoadingSlice";
+import Container from "../components/common/Container";
 
 const MoviesPage = () => {
-  return (
-    <div>MoviesPage</div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default MoviesPage
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      dispatch(setGlobalLoading(true));
+      const { response, error } = await movieApi.getAllMovies();
+      dispatch(setGlobalLoading(false));
+
+      if (response) setMovies(response);
+      if (error) toast.error(error.message);
+    };
+
+    getMovies();
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Toolbar />
+      <Box sx={{ ...uiConfigs.style.mainContent }}>
+        <MovieGrid movies={movies} />
+      </Box>
+    </div>
+  );
+};
+
+export default MoviesPage;
